08a145b708b0cae19a4f13c1b700f1fc05f102dc
Firdevs Akbayır <77115759+lyushher@users.noreply.github.com>
2022-06-24 12:04:31 +0300

    Update GenerateQRWithLogo.java

--- GenerateQRWithLogo.java
+++ GenerateQRWithLogo.java
@@ -2,12 +2,12 @@ import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import javax.imageio.ImageIO;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import javax.imageio.ImageIO;
 
 public class GenerateQRWithLogo {
  
